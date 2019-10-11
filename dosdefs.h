0f0964e7fbac283bcfe3313b346669e5adef5706
Kamil Rytarowski <n54@gmx.com>
2019-10-11 03:23:56 +0200

    Initial import of TinyTcl the latest sourceforge CVS checkout
    Tiny Tcl 6.8 is a rommable, minimal Tcl for embedded applications.
    
    Derived from the venerable Tcl 6.7 release, Tiny Tcl 6.8 has a solid Tcl
    feature set, excluding newer capabilities of Tcl 7 and 8 such as the
    bytecode compiler, namespaces, sockets, and async event handling, among others.
    
    Still, major applications have been written in Tcl 6.
    
    Excluding C library functions, Tiny Tcl compiles down to less than 60 Kbytes
    on most machines, far smaller than any Tcl 7 or Tcl 8 derivatives.

---
+++ dosdefs.h
@@ -0,0 +1,26 @@
+/* 
+ * dosdefs.h --
+ *
+ * Control compilation of various DOS functions.
+ *
+ * $Id: dosdefs.h,v 1.1 2001/04/30 14:32:53 karll Exp $
+ */
+
+/* define if you want bios_memsize extension */
+#define COMPILE_BIOS_MEMSIZE
+
+/* define if you want bios_equiplist extension */
+#define COMPILE_BIOS_EQUIPLIST
+
+/* define if you want diskfree extension */
+#define COMPILE_DISKFREE
+
+/* define if you want getfat extension */
+#define COMPILE_GETFAT
+
+/* define if you want com extension */
+#define COMPILE_BIOS_SERIALCOM
+
+/* define if you want getverify extension */
+#define COMPILE_GETVERIFY
+
